cf4491145dda6398ded300e0f21b64ae96c47879
rvarca-ups <ryan.varca1@marist.edu>
2023-07-12 16:54:32 -0400

    Hackathon Day 1 End

--- project_Backend/connect.db.py
+++ project_Backend/connect.db.py
@@ -1,9 +1,10 @@
 #Imports only work in WSL (for me)
+import mysql.connector
+from mysql.connector import Error
+
 #USER INFO:
 #   usr: nuggets@localhost
 #   ID: Cnuggets8@
-import mysql.connector
-from mysql.connector import Error
 
 
 def create_server_connection(host_name, user_name, user_password):
@@ -20,10 +21,75 @@ def create_server_connection(host_name, user_name, user_password):
 
     return connection
 
-
 connection = create_server_connection("localhost", "nuggets@localhost", "Cnuggets8@")
 
 myConn = connection.cursor()
 
-#Example execute commands
-#myConn.execute("CREATE DATABASE IF NOT EXISTS Pydb")
+myConn.execute("USE The_Chicken_Nuggets")
+
+
+
+
+
+'''
+myConn.execute("SELECT * FROM package_table")
+
+for x in myConn:
+    print(x)
+
+'''
+
+
+'''
+#INSERT INTO employees
+emp_ID = input("Enter your UPS employee ID \n")
+car_ID = input("Enter your car's make and model (pick 1-20) \n")
+personal_items = input("Enter the amount of space of any personal items in your car \n")
+
+myConn.execute("INSERT INTO employees VALUES(" + emp_ID + "," + car_ID + "," + personal_items + ")")
+myConn.execute("SELECT * FROM employees")
+
+for x in myConn:
+    print(x)
+
+myConn.execute("COMMIT")
+
+'''
+#select employees.employee_ID, employees.personal_items, vehicle_data.trunk_space, vehicle_data.foldable_seat_space FROM employees LEFT JOIN vehicle_data ON employees.car_ID = vehicle_data.car_ID;
+
+myConn.execute("COMMIT")
+
+emp_ID = input("Enter your UPS employee ID \n")
+
+myConn.execute("select employees.employee_ID, employees.personal_items, vehicle_data.trunk_space, vehicle_data.foldable_seat_space FROM employees LEFT JOIN vehicle_data ON employees.car_ID = vehicle_data.car_ID where employee_ID = " + emp_ID + ";")
+rs = myConn.fetchall()
+trunkSpace = rs[0][2]
+foldable_space = rs[0][3]
+personal_Items = rs[0][1]
+
+totalSpace = trunkSpace + foldable_space - personal_Items
+
+print(totalSpace)
+
+myConn.execute("SELECT * FROM packages ORDER BY package_ID")
+rs = myConn.fetchall()
+
+rsEmpty = 0
+remainingSpace = totalSpace
+emptyList = []
+packageCount = 0
+
+while rsEmpty == 0 and remainingSpace > 0:
+    a = myConn.execute("select volume, package_ID from packages left join package_types on packages.package_type = package_types.package_type where shipped = 0 and volume <= "+str(remainingSpace)+" order by volume desc;")
+    rs1 = myConn.fetchall()
+    if rs1 == emptyList:
+        rsEmpty = 1
+    else:
+        packageCount = packageCount + 1
+        remainingSpace = remainingSpace - rs1[0][0]
+        pkgID = rs1[0][1]
+        print("You will take package "+pkgID)
+        myConn.execute("update packages set shipped = 1 where package_ID = "+"\""+pkgID+"\"")
+        myConn.execute("commit")
+print("You are delivering "+str(packageCount)+" packages.")
+
